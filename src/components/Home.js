dedd44a12c7943e3fa2be0b49eb3e8da181c1032
Elvin <Elvin@ElvinGarcia.com>
2022-04-03 17:42:41 -0400

    added necessary text to pass test

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>home</h1>
+      <h1>Home Page</h1>
       <p>A List of Moives, Directors and their movies</p>
     </div>
   );
