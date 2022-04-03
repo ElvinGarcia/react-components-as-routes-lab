b9a62010f7dd511d6dce3ea179a43d0a1e516a3c
Elvin <Elvin@ElvinGarcia.com>
2022-04-03 16:06:29 -0400

    completed NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+
+const link = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
+  <div>
+    <NavLink
+        exact to="/actors"style={link}  activeStyle={{
+            background: "darkblue",
+          }}>
+        Actors
+    </NavLink>
+
+    <NavLink
+        exact to="/movies"style={link}  activeStyle={{
+            background: "darkblue",
+          }}>
+        Movies
+    </NavLink>
+
+    <NavLink
+        exact to="/directors"style={link}  activeStyle={{
+            background: "darkblue",
+          }}>
+        Directors
+    </NavLink>
+
+    <NavLink
+        exact to="/home"style={link}  activeStyle={{
+            background: "darkblue",
+          }}>
+        Home
+      </NavLink>
+  </div>
   );
 };
 
