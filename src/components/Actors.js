d2c69f3357345c4ffaeb9a8b35c4d1bf36fdae75
Elvin <Elvin@ElvinGarcia.com>
2022-04-03 17:32:19 -0400

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor) => (
+        <div key={actor.name}>
+          <h3>{actor.name}</h3>
+          <ul key={actor.name}>
+          {actor.movies.map((movie) => (
+            <li key={movie}>{movie}</li>
+          ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
